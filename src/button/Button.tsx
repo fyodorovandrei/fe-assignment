f7e8581922b9694d63906c9bdb7edefa1c08c98b
Andrei Fiodorov <andrei.fiodorov@icloud.com>
2023-01-19 12:46:59 +0100

    implement button component

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -17,4 +17,44 @@
 // We are using css modules in this project, so you can add styles from the button module file with styles[className] - https://github.com/css-modules/css-modules
 // You may add any helper libraries you need
 
-export const Button = () => {};
+import React, {SVGProps} from "react";
+import classNames from "classnames";
+
+import styles from "./_button.module.css";
+
+const VARIANT_PRIMARY = "primary";
+const VARIANT_SECONDARY = "secondary";
+
+type Variant = typeof VARIANT_PRIMARY | typeof VARIANT_SECONDARY;
+
+type Props =  React.HTMLProps<HTMLButtonElement> & {
+    type?: "button" | "submit" | "reset" | undefined;
+    variant?: Variant;
+    Icon?: React.FunctionComponent<SVGProps<SVGSVGElement>>;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, Props>((
+    {
+        children,
+        className,
+        variant = "primary",
+        Icon,
+        ...props
+    },
+    ref
+) => {
+    const cssClasses = classNames(styles.button, {
+        [styles.primary]: variant === "primary",
+        [styles.secondary]: variant === "secondary",
+    })
+
+    return (
+        <button {...props} ref={ref} className={`${cssClasses} ${className}`}>
+            <span>{children}</span>
+            {Icon && <Icon className={styles.icon} />}
+        </button>
+    );
+});
+
+export default Button;
+
