35c52c67b435e8f77ca8027e51126bbb83b19410
Andrei Fiodorov <andrei.fiodorov@icloud.com>
2023-01-19 12:47:22 +0100

    add example of buttons on homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import './App.css';
+import Button from "./button/Button";
+import { ReactComponent as Icon } from "./button/umbrella.svg";
+import styles from "./App.module.css";
 
 function App() {
-  return (
-    <div className="App">
-    <p>Add your buttons here, both variants plus disabled</p>
-    </div>
-  );
+	return (
+		<div className={styles.App}>
+			<div className={styles.col}>
+				<Button>Primary</Button>
+				<Button Icon={Icon}>Primary</Button>
+				<Button disabled>Primary Disabled</Button>
+				<Button Icon={Icon} disabled>Primary Disabled</Button>
+			</div>
+			<div className={styles.col}>
+				<Button variant="secondary" >Secondary</Button>
+				<Button variant="secondary" Icon={Icon}>Secondary</Button>
+				<Button variant="secondary" disabled>Secondary Disabled</Button>
+				<Button variant="secondary" Icon={Icon} disabled>Secondary Disabled</Button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
